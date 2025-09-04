3a91a878117464e759573766b62fb0db6df58bf9
Stepan <korablikiuu@gmail.com>
2025-09-04 18:50:48 +0300

    first title

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+        <div className="flex-1 pt-36 padding-x">
+            <h1 className="hero__title">
+                Find, book, or rent a car
+            </h1>
+            <p className="hero__subtitle">
+                Streamline your car rental experince with our very very uniqe...
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
